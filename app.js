feded0c3e6a84bf39772eb11083da10a139e86f1
楼天楠 <tnlou@justhealth.cn>
2022-07-04 15:14:53 +0800

    chore: 系统入口文件

--- app.js
+++ app.js
@@ -1,44 +1,51 @@
-const Koa = require('koa')
-const app = new Koa()
-const json = require('koa-json')
-const onerror = require('koa-onerror')
-const bodyparser = require('koa-bodyparser')
-const logger = require('koa-logger')
+const Koa = require('koa');
+const app = new Koa();
+const json = require('koa-json');
+const onerror = require('koa-onerror');
+const bodyparser = require('koa-bodyparser');
+const logger = require('koa-logger');
 const cors = require('koa2-cors');
 
-
-const weapp = require('./controller/weapp.controller')
-const user = require('./controller/user.controller')
+const weapp = require('./controller/weapp.controller');
+const user = require('./controller/user.controller');
+const movie = require('./controller/movie.controller');
+const banner = require('./controller/banner.controller');
+const article = require('./controller/article.controller');
 
 // error handler
-onerror(app)
+onerror(app);
 
 // middlewares
-app.use(bodyparser({
-  enableTypes:['json', 'form', 'text']
-}))
-app.use(json())
-app.use(logger())
-app.use(require('koa-static')(__dirname + '/public'))
+app.use(
+  bodyparser({
+    enableTypes: ['json', 'form', 'text'],
+  }),
+);
 
+app.use(json());
+app.use(logger());
+// app.use(require("koa-static")(__dirname + "/public"));
 
 // logger
 app.use(async (ctx, next) => {
-  const start = new Date()
-  await next()
-  const ms = new Date() - start
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
-})
+  const start = new Date();
+  await next();
+  const ms = new Date() - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+});
 
 // routes
-app.use(weapp.routes(), weapp.allowedMethods())
-app.use(user.routes(), user.allowedMethods())
+app.use(weapp.routes(), weapp.allowedMethods());
+app.use(user.routes(), user.allowedMethods());
+app.use(movie.routes(), movie.allowedMethods());
+app.use(banner.routes(), banner.allowedMethods());
+app.use(article.routes(), article.allowedMethods());
 
 // error-handling
 app.on('error', (err, ctx) => {
-  console.error('server error', err, ctx)
+  console.error('server error', err, ctx);
 });
 
 app.use(cors());
 
-module.exports = app
+module.exports = app;
